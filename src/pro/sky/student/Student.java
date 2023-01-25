405ad880c320ed672bcff92a822914d368e663ca
letuu <albertvt@ya.ru>
2023-01-25 23:22:28 +0300

    Курсовая 6 - Задача

--- src/pro/sky/student/Student.java
+++ src/pro/sky/student/Student.java
@@ -1,6 +1,58 @@
 package pro.sky.student;
 
 public class Student {
+
+    //Курсовая 6 - Задача
+    //Напишите пример перехвата и обработки исключения с использованием собственных исключений
+
+    private String name;
+
+    private boolean isHomeWorkDone;
+    private boolean isCourseWorkDone;
+    private boolean isTicketsLearned;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
     public static void main(String[] args) {
+
+        Student student = new Student("Vladimir");
+        student.submitHomeWork();
+//        student.submitCourseWork();
+        student.learnTickets();
+        try {
+            student.exam();
+        } catch (StudentIsNotReadyException e) {
+            System.out.println(e.getMessage());
+            System.out.println("\nПроверяем допуски к экзамену:\n"
+                    + "Домашняя работа сдана? " + student.isHomeWorkDone
+                    + "\nКурсовая работа сдана? " + student.isCourseWorkDone
+                    + "\nБилеты выучены? " + student.isTicketsLearned);
+        }
+    }
+
+    public void submitHomeWork() {
+        System.out.println("Домашняя работа сдана!");
+        this.isHomeWorkDone = true;
+    }
+
+    public void submitCourseWork() {
+        System.out.println("Курсовая работа сдана!");
+        this.isCourseWorkDone = true;
+    }
+
+    public void learnTickets() {
+        System.out.println("Билеты выучены!");
+        this.isTicketsLearned = true;
+    }
+
+    public void exam() throws StudentIsNotReadyException {
+        System.out.println("Идем на экзамен?");
+        if (isHomeWorkDone && isCourseWorkDone && isTicketsLearned) {
+            System.out.println("Студент " + name + " готов к экзамену!");
+        } else {
+            throw new StudentIsNotReadyException("Студент " + name + " не допущен к экзамену! Проверьте допуски!");
+        }
     }
 }
---
+++ src/pro/sky/student/StudentIsNotReadyException.java
@@ -0,0 +1,8 @@
+package pro.sky.student;
+
+public class StudentIsNotReadyException extends Exception {
+
+    public StudentIsNotReadyException(String message) {
+        super(message);
+    }
+}
